46e0d27f9c859a4efeb5c68a3b55292ba45bde4b
fant1-LB <fantinpro84@gmail.com>
2026-03-12 14:41:14 +0100

    relecture alice

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ def accueil():
                 ayant recours au <i> machine-learning</i>. Ce guide ne traitera pas de formes d'IA tel que les forêts de
                 décision ou les chaînes de Markov.</p>
              
-             <p> Ce guide est destiné à des personnes ayant des niveaux informatiques variables, et est pensé pour n'avoir aucun prérequis technique d'entrée, si certains termes vous sont étrangers n'hésitez pas à consulter notre lexique.
+             <p> Ce guide est destiné à des personnes ayant des niveaux informatiques variables, et est pensé pour n'avoir aucun prérequis technique d'entrée. Si certains termes vous sont étrangers n'hésitez pas à consulter notre lexique.
             Du fait de l'évolution très rapide des technologies dans ce secteur certains des projets et certaines des applications présentées dans ce guide peuvent être obsolètes au moment de la lecture.
             En introduction de ce guide nous voulons également insister sur le fait que l'"IA" ne doit pas être une fin en soi, et devrait être utilisée comme un outil pour accomplir une mission.
             À ce titre, des questions telles que le régime de droit des données traitées, le coût économique, écologique ou l'accompagnement au changement doivent être réflechies de la même manière que pour n'importe quel outil numérique.</p>
@@ -39,12 +39,10 @@ def accueil():
 def lexique ():
     st.html('''<h3 id="API">API</h3>
             <p>Une API, pour <i>application programming interface</i> ou « interface de programmation d'application »
-                est une interface qui permet de connecter un logiciel à une machine en ligne
-                pour y effectuer des requêtes. Les APIs permettent par exemple d'envoyer sur un serveur extérieur des
-                requêtes à des modèles d'IA trop lourds pour tourner sur notre propre machine.
-                Un code, même executé localement, qui implique un appel d'API, partagera des données avec un serveur
-                extérieur.
-                La plupart des grands <i>Large language models</i> commerciaux peuvent être requêtés via leur API. </p>
+                est un système qui permet de connecter un logiciel à une machine en ligne. Les APIs permettent par exemple d'envoyer sur un serveur extérieur des
+                requêtes à des modèles d'IA trop lourds pour fonctionner sur un ordinateur ou serveur de l'institution.
+                Un traitement qui implique de faire appel à une API implique donc l'accès par un acteur extérieur à l'institution aux documents traités, et donc une vigilance particulière quant au régime de droit des documents en question.
+                La plupart des grands <i>Large language models</i> commerciaux peuvent être utilisés via leur API. </p>
             <h3 id="Annotations">Annotations</h3>
             <p>Dans un contexte IA, les annotations sont les données que l’on va utiliser pour entraîner ou réentrainer
                 un modèle. La forme de l’annotation dépend du modèle que l’on veut entraîner ou réentrainer. La
@@ -69,33 +67,32 @@ def lexique ():
                 de données spécifiquement annotées par un humain (approche supervisée) ou brutes (approche
                 non supervisée). Cette étape demande une puissance de calcul
                 considérable. Le réentraînement est le fait de faire subir un nouvel entraînement à un modèle existant
-                pour le modifier et l’adapter à nos besoins spécifiques.</p>
+                pour le modifier et l’adapter à des besoins spécifiques.</p>
             <h3 id="Finetuning">Finetuning</h3>
             <p>Le <em>finetuning</em> est l’ensemble des manipulations que l’on fait sur un modèle pour améliorer ses
                 résultats. Le <em>finetuning</em> peut passer par la modification de paramètres, d’un <em>prompt</em>,
                 ou encore par un réentraînement.</p>
             <h3 id="GPU">GPU</h3>
-            <p>Un GPU, ou <em>Graphic Processing Unit</em> est une unité de calcul assurant les fonctions de calcul
-                d’image. Originellement les GPUs ont été développés pour les jeux vidéos et le calcul de déplacements en
-                plusieurs dimensions. Avec l’émergence de l’IA, la communauté scientifique s’est rendue compte qu’ils
-                étaient bien plus efficaces pour le calcul des vecteurs qu’utilisent la plupart des modèles d’IA que les
+            <p>Un GPU, ou <em>Graphic Processing Unit</em> est une unité de calcul assurant les fonctions de traitement des
+                d’images. Le GPU est le coeur de ce qu’on appelle plus couramment les cartes
+                graphiques. Originellement les GPUs ont été développés pour les jeux vidéos et le calcul de trajectoires. Avec l’émergence de l’IA, la communauté scientifique s’est rendue compte qu’ils
+                étaient bien plus efficaces pour le calcul des objets mathématiques (vecteurs) qu’utilisent la plupart des modèles d’IA que les
                 CPUs (ou processeurs) traditionnels de nos ordinateurs. L’utilisation de GPUs accélère grandement le
                 traitement par IA de données. Avoir un GPU est même nécessaire pour les modèles les plus lourds et les
-                entraînements/réentraînements. Le GPU est le coeur de ce qu’on appelle plus couramment les cartes
-                graphiques.</p>
+                entraînements et réentraînements. </p>
             <h3 id="LLM">LLM</h3>
-            <p>Un LLM, ou grand modèle de langue, est un modèle IA possédant un grand nombre de paramètres et capable d'interpréter et génerer du langage naturel. Il est entraîné sur des grandes quantités de texte.<br>
+            <p>Un LLM, ou grand modèle de langue, est un modèle IA capable d'interpréter et génerer du langage naturel.<br>
                 Un modèle de langue est un modèle probabiliste basé sur la distribution d’éléments linguistiques (lettres,
                 phonèmes, mots) dans une langue naturelle. Les plus connus sont des modèles génératifs qui calculent le
-                mot suivant ou la lettre suivante dans une séquence de mots, selon un contexte, pour interagir avec
-                l’utilisateur. Certains LLMs sont également capables de traiter des images, on les appelle alors aussi
+                mot suivant ou la lettre suivante le plus probablent dans une séquence de mots, selon un contexte, pour interagir avec
+                l’utilisateur. Un grand modèle de langue se différencie d'un modèle de langue classique par la masse de son corpus d'entraînement et des ses paramètres, l'unité utilisée pour mesurer la taille d'un modèle d'IA.Certains LLMs sont également capables de traiter des images, on les appelle alors aussi
                 des VLM (<i>vision-language model</i>).<br>
                 Ex : GPT-4, DeepSeek-R1, Llama-3 …</p>
             <h3 id="Modèle-IA">Modèle IA</h3>
             <p>Un modèle IA est un algorithme capable (avec plus ou moins d’efficacité) d’effectuer un ensemble de
                 tâches pour lesquelles on l’a entraîné. Il reçoit un type de données en entrée, et en propose un autre
                 en sortie. Le terme “modèle” s’applique en IA peu importe le domaine. On appelle un modèle qui peut
-                recevoir plusieurs types de données en entrée (par exemple texte ET image), un modèle
+                recevoir plusieurs types de données en entrée (par exemple texte ET image) un modèle
                 <em>multimodal</em>. On distingue aussi généralement les modèles <em>spécialisés</em>, capables de
                 réaliser une unique tâche (par exemple détecter les visages sur une image), des modèles
                 <em>généralistes</em>, ou <em>modèles de fondation</em> capables de réaliser des tâches ou de traiter des données très variées (comme la plupart des LLMs génératifs grand public). Les
@@ -110,7 +107,7 @@ def lexique ():
                 l’HTR). Ces deux technologies peuvent être rassemblées sous le terme d'"ATR", pour <em> Automatic Text Recognition </em> <br>
                 Ex : Tesseract-ocr, pero-ocr, monkey-ocr…</p>
             <h3 id ="Plongement">Plongement </h3>
-            Un plongement, ou <em> embedding </em> est un objet mathématique complexe, créé et interprété par un modèle pour manipuler des objets, concepts, images...  réels.
+            Un plongement, ou <em> embedding </em> est un objet mathématique complexe, créé et interprété par un modèle pour manipuler les données qu'on lui donne à traiter.
             <h3 id="Post-correction">Post-correction</h3>
             <p>La post-correction est le travail que l’on fait après l’application d’un modèle IA pour corriger les
                 erreurs qu’il commet. Selon les situations la post-correction sera nécessaire ou non. Selon les
@@ -154,9 +151,9 @@ Les modèles de classification, eux, vont, comme leur nom l'indique, associer à
         
         st.image(image=image_path("excelsior.jpg"), caption="Extraction par segmentation des photographies d'une page de journal (Excelsior du 24 novembre 1910 - ark:/12148/bpt6k4600008j) à l'aide d'un modèle YOLO")        
                 
-        st.markdown('''La classification est utile pour catégoriser des images sur des corpus massifs mais relativement homogènes. En l'état ces modèles ne sont pas fiables sur un trop grand nombre de classes, mais fonctionnels sur un petit nombre. Comme la segmentation, les modèles de type YOLO ou Dino peuvent réaliser des classifications. On peut également utiliser certains VLM/LLM, comme Florence, ou Qwen pour ces tâches, mais obtenir des sorties structurées et controlables de ces modèles demande un travail plus fin.
+        st.markdown('''La classification est utile pour catégoriser des images sur des corpus massifs mais relativement homogènes. Comme la segmentation, les modèles de type YOLO ou Dino peuvent réaliser des classifications. On peut également utiliser certains VLM/LLM, comme Florence, ou Qwen pour ces tâches, mais obtenir des sorties structurées et contrôlables de ces modèles demande un travail plus fin.
 
-La description et l'indexation d'images sont deux objets problématiques loin d'être résolus au moment d'écriture de ce guide. La description est envisageable avec des modèles type VLM, mais pose le problème fondamental de la mesurabilité des résultats. L'indexation sur plusieurs niveaux à l'aide de thésaurus complexes comme le thésaurus Garnier ou IconClass pose un problème différent, qui est l'inadaptation des modèles d'IA contemporains à ces plusieurs niveaux d'indexation. On peut envisager un traitement par plusieurs modèles de classification pour imiter les différents niveaux d'un thésaurus, mais il faut noter que la multiplication des traitements multiplie le nombre d'erreurs, et chaque entraînement a un coût technique et écologique qui doit être pris en compte. Pour ces deux usages, s'il ne faut pas exclure les traitement entièrement automatisés, sur des quantités raisonnables d'images nous recommanderions plutôt d'avoir recours à l'IA pour appuyer le personnel scientifique, dans le cadre de traitements semi-automatiques.
+La description et l'indexation d'images sont deux objets problématiques loin d'être résolus au moment d'écriture de ce guide. La description est envisageable avec des modèles type VLM, mais pose le problème fondamental de la mesurabilité des résultats. L'indexation sur plusieurs niveaux à l'aide de thésaurus complexes comme le thésaurus Garnier ou IconClass pose un problème différent, qui est l'inadaptation des modèles d'IA contemporains à leurs plusieurs niveaux d'indexation. On peut envisager un traitement par plusieurs modèles de classification pour imiter les différents niveaux d'un thésaurus, mais il faut noter que la multiplication des traitements multiplie le nombre d'erreurs, et chaque entraînement a un coût technique et écologique qui doit être pris en compte. Pour ces deux usages, s'il ne faut pas exclure les traitement entièrement automatisés, sur des quantités raisonnables d'images nous recommanderions plutôt d'avoir recours à l'IA pour appuyer le personnel scientifique, dans le cadre de traitements semi-automatiques.
 
 ### Outils et ressources
 
@@ -165,7 +162,7 @@ De nombreux outils et ressources existent à propos de la segmentation et de la 
 *   [L'article en profondeur d'IBM consacré aux différents types de segmentation.](https://www.ibm.com/fr-fr/think/topics/image-segmentation)
 *   [Roboflow, plateforme en ligne pour la segmentation d'image. La plateforme dispose d'options gratuites et payantes, selon les besoins qu'on a.](https://roboflow.com/) : :us: - :lock: - :free:/:moneybag:
 *   [La Google Teachable Machine, application en ligne pour expérimenter avec la classification d'images.](https://teachablemachine.withgoogle.com/train) : :us: - :lock: - :free:
-*   [Aikon, plateforme de l'ERC discover à l'école nationale des ponts et chaussées pour la segmentation de manuscrits.](https://aikon-platform.github.io/) : :fr: - :unlock: - :free:
+*   [Aikon, plateforme de l'ERC discover à l'Ecole Nationale des Ponts et Chaussées pour la segmentation de manuscrits.](https://aikon-platform.github.io/) : :fr: - :unlock: - :free:
 *   [Arkindex, plateforme développée par l'entreprise Teklia, pour le traitement en masse de documents numérisés.](https://www.teklia.com/arkindex) : :fr: - :unlock: - :free:/:moneybag:
 *   [Labelstudio, plateforme d'annotation développée par l'entreprise Human Signal pour l'annotation d'images pour différentes formes de segmentation.](https://labelstud.io/) : :us: - :unlock: - :free:/:moneybag:
 
@@ -191,11 +188,11 @@ Cette section décrira les usages possibles de technologies IA pour transcrire a
 
 
 
-Ce sont des technologies plus anciennes et plus éprouvées que la segmentation, ou encore les chatbots présentés dans ce guide. Comme mentionné précédemment la transcription se sépare en deux grandes catégories : l'OCR et l'HTR. L'OCR se consacre à la reconnaissance de caractères imprimés, l'HTR à la reconnaissance de caractères manuscrits.''', text_alignment = "justify")
+Ce sont des technologies plus anciennes et plus éprouvées que la segmentation, ou encore les chatbots présentés dans ce guide. La transcription se sépare en deux grandes catégories : l'OCR et l'HTR. L'OCR se consacre à la reconnaissance de caractères imprimés, l'HTR à la reconnaissance de caractères manuscrits.''', text_alignment = "justify")
         
         st.image(image=image_path("OCR_Hugo_BnF.png"), caption="capture d'écran montrant un résultat d'un OCR sur un texte de Victor Hugo")
         
-        st.markdown('''Du fait de la nature très répétitive, et de la relative persistance dans le temps des polices imprimées, les modèles d'OCR sont généralement applicables à un large éventail de documents. L'usage de cette technologie se fait donc plus facilement à partir de solutions pré-existantes, déjà développées dans le cadre de projets de recherche précédents, ou pour le traitement de texte contemporain. Plus que la transcription même, la difficulté va résider dans la structuration des transcriptions si la mise en page du document est complexe. A l'inverse, l'HTR, du fait de la plus grande variabilié des écritures manuscrites, en particulier dans le temps, va généralement demander un travail de spécialisation des modèles, même si les VLMs les plus récents viennent remettre en cause ce paradigme. Dans les deux cas, il faudra prévoir une "vérité terrain", c'est à dire quelques documents représentatifs du corpus traité transcrits à la main, pour mesurer la fiabilité de l'instrument d'OCR ou d'HTR utilisé. La qualité d'une transcription est généralement mesurée à partir du CER (Character Error Rate), c'est à dire le pourcentage de caractères faux dans la transcription par rapport à la "vérité terrain", ou le WER (Word Error Rate), qui est de son côté le pourcentage de mots faux.
+        st.markdown('''Du fait de la nature très répétitive, et de la relative persistance dans le temps des polices imprimées, les modèles d'OCR sont généralement applicables à un large éventail de documents. L'usage de cette technologie se fait donc plus facilement à partir de solutions pré-existantes, déjà développées dans le cadre de projets de recherche précédents, ou pour le traitement de texte contemporain. Plus que la transcription même, la difficulté va résider dans la structuration des transcriptions si la mise en page du document est complexe. A l'inverse, l'HTR, du fait de la plus grande variabilié des écritures manuscrites, en particulier dans le temps, va généralement demander un travail de spécialisation des modèles, même si les VLMs les plus récents viennent remettre en cause ce paradigme. Dans les deux cas, il faudra prévoir une "vérité terrain", c'est à dire quelques documents représentatifs du corpus traité transcrits à la main, pour mesurer la fiabilité de l'instrument d'OCR ou d'HTR utilisé. La qualité d'une transcription est généralement mesurée à partir du CER (<em>Caracter Error Rate</em>), c'est à dire le pourcentage de caractères faux dans la transcription par rapport à la "vérité terrain", ou le WER (<em>Word Error Rate</em>), qui est de son côté le pourcentage de mots faux.
 
 
 
@@ -203,7 +200,7 @@ La mise en place de ces technologies dépendra de vos objectifs, de vos compéte
 
 Il est important de noter que la qualité des résultats dépendra fortement de la qualité des images numérisées et des spécificité des documents (langue, écriture, mise en page...).
 
-En terme d'infrastructures il faut prévoir de la puissance de calcul pour l'application sur de larges corpus, et pour l'entraînement ou réentraînement de modèles spécialisés. Pour les moyens humains, une personne qui parle la langue, et est capable de lire l'écriture du document est évidemment indispensable ne serait-ce que pour établir la "vérité terrain". Il ne faut pas attendre de la machine d'être capable de transcrire ce qu'aucun humain ne saurait lire.''', text_alignment ="justify")
+Comme pour la plupart des modèles d'IA, il faut prévoir de la puissance de calcul pour l'application sur de larges corpus, et pour l'entraînement ou réentraînement de modèles spécialisés. Pour les moyens humains, une personne qui parle la langue, et est capable de lire l'écriture du document est évidemment indispensable ne serait-ce que pour établir la "vérité terrain". Il ne faut pas attendre de la machine d'être capable de transcrire ce qu'aucun humain ne saurait lire.''', text_alignment ="justify")
         st.image(image=image_path("infographie_difficultes_htr_ocr.JPG"), caption="Exemple de documents et des difficultés posées par leur transcription")
 
         st.markdown('''### Usages de transcription automatique
@@ -245,10 +242,9 @@ Si les technologies évoluent très vite, au moment d'écriture de ce guide nous
 def projets():
     st.html("to be done")
 
-page_names_to_func = {"Accueil": accueil,
-                      "Lexique": lexique,
-                      
+page_names_to_func = {"Accueil": accueil, 
                       "Capacités de l'IA": capacites,
+                      "Lexique": lexique,
                       "Ajouter un projet":add_project,
                       "Projets":afficher_projets}
 
