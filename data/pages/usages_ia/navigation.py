c2ffd131eda389685e9b59910da78dd219d8ec29
Pierrousson <pierre.husson.56@gmail.com>
2026-04-17 12:23:38 +0200

    navigation

--- data/pages/usages_ia/navigation.py
+++ data/pages/usages_ia/navigation.py
@@ -14,7 +14,7 @@ def usage_navigation_text():
         st.image(image=image_path("pixplot.png"), caption= "Visualisation globale de 11974 peintures italiennes du RETIF (INHA) grâce au logiciel PixPlot", width=800)
         
     st.markdown("Grâce à des algorithmes de similarité et d’analyse d’images, les outils d’intelligence artificielle rendent possible l’exploration de vastes corpus iconographiques, même dépourvus de métadonnées. Ils présentent un intérêt majeur pour les collections de photographies, d’arts graphiques ou d’objets, en facilitant l’identification de motifs récurrents, la classification des œuvres selon différents critères et la rédaction de descriptions ou leur indexation.")
-    st.markdown("Ces outils permettent d’identifier des « clusters » (ou « grappes ») au sein d’un corpus d’images, c’est-à-dire des ensembles d’images présentant des similarités visuelles ou sémantiques.  Par ailleurs, ces outils se révèlent également utiles pour repérer d’éventuels doublons au sein d'un corpus. Une fois les logiques sous jacentes à un cluster identifiées par l'utilisateur ou utilisatrice (couleurs, textures, composition...), l'annotation de l'intégralité du cluster permet de gagner beaucoup de temps par rapport à une annotation manuelle.")
+    st.markdown("Ces outils permettent d’identifier des « clusters » (ou « grappes ») au sein d’un corpus d’images, c’est-à-dire des ensembles d’images présentant des similarités visuelles ou sémantiques. Par ailleurs, ces outils se révèlent également utiles pour repérer d’éventuels doublons au sein d'un corpus. Une fois les logiques sous jacentes à un cluster identifiées par l'utilisateur ou utilisatrice (couleurs, textures, composition...), l'annotation de l'intégralité du cluster permet d'accélérer l'identification des images par rapport à une annotation manuelle.")
 
     st.markdown("### Rechercher des images en langage naturel")
 
@@ -28,7 +28,7 @@ def usage_navigation_text():
             caption="Recherche via l'application Panoptic, et un modèle CLIP, des images associées au mot tennis dans le dataset bbc_news_alltime, qui contient l'intégralité des miniatures du site web de la BBC, sur huggingface.",
             width=800
         )
-    st.markdown("Certains modèles permettent également d’effectuer des recherches au sein d’images non documentées à l’aide de langage naturel. Entraînés à associer des images à des descriptions textuelles, ils peuvent ensuite être appliqués à de nouveaux corpus iconographiques afin d’en faciliter l’exploration et la description. C'est par exemple le cas de modèles comme CLIP ou Siglip")
+    st.markdown("Certains modèles, comme CLIP (OpenAI) ou SigLIP (Google DeepMind), permettent également d’effectuer des recherches au sein d’images non documentées à l’aide de langage naturel. Entraînés à associer des images à des descriptions textuelles, ils peuvent ensuite être appliqués à de nouveaux corpus iconographiques afin d’en faciliter l’exploration et la description. ")
 
     st.markdown('''### Outils et ressources
 
